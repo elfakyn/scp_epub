e987ec9fee6a84473207c7bce5b7653ce93ab7c9
elfakyn <git@sidhail.com>
2020-05-24 06:23:40 -0400

    parameterize web html component test, update notes

--- scp_epub/test_component/process/test_process_page.py
+++ scp_epub/test_component/process/test_process_page.py
@@ -16,12 +16,16 @@ class TestProcessPage(unittest.TestCase):
     def setUp(self):
         self.maxDiff = 0
 
-    def test_process_page(self):
+    @parameterized.expand([
+        [
+            'SCP-1257',
+            'scp-1257.html',
+            'scp-1257_converted.html',
+            ['scp-173', 'scp-682'],
+        ],
+    ])
+    def test_process_page(self, expected_page_title, expected_web_html_file, expected_processed_html_file, expected_url_allow_list):
         # Arrange
-        expected_url_allow_list = None
-        expected_page_title = 'SCP-1257'
-        expected_web_html_file = 'scp-1257.html'
-        expected_processed_html_file = 'scp-1257_converted.html'
 
         with open(os.path.join(os.path.dirname(__file__), constants.test.TEST_COMPONENT_PROCESS_PAGE_CASES_DIR, expected_web_html_file), 'r', encoding=constants.constants.ENCODING) as target_file:
             expected_web_html = target_file.read()
